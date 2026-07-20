e889b2e94d4ef8d4795334807e07dd3edcfac838
Biberacher <philneilsharma@gmail.com>
2026-07-20 21:50:53 +0000

    Erste Flask Website erstellt

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Versteckspiel startet!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def startseite():
+    return """
+    <h1>Hide & Hunt</h1>
+    <p>Das Versteckspiel läuft!</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    
